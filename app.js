c3a5b0b9613d9a0f8f6f4656e08d5fa782ee4245
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-11-07 04:21:10 -0800

    Changed grid layout in div with options class. Also working on js functionality

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // Selecting elements from dom
 const nameInput = document.querySelector('#userName');
 const newTodoForm = document.querySelector('#new-todo-form');
-
-
-
+// Function to save new todo to local storage
+const saveLocalTodos = (el) => {
+    // Setting todos to localstorage data or empty array
+    if(localStorage.getItem('todos') == null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    // Appending new todo 'el' to todos array
+    todos.push(el);
+    // Saving new todos to localstorage
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
 // Loading todos from localstorage and setting username
 window.addEventListener('DOMContentLoaded', () => {
     // Setting username to localstorage data or empty string
@@ -25,8 +35,20 @@ window.addEventListener('DOMContentLoaded', () => {
 nameInput.addEventListener('change', (e) => {
     localStorage.setItem('username', e.target.value);
 });
+// Eventlistener to add new todo to list
 newTodoForm.addEventListener('submit', (e) => {
+    // Preventing form page from refreshing
     e.preventDefault();
-
-    console.log('form submitted');
+    // New todo
+    // Getting values from name attr
+    const todo = {
+        content: e.target.elements.content.value,
+        category: e.target.elements.category.value,
+        done: false,
+        createdAt: new Date().getTime()
+    }
+    // Saving new todo to local storage
+    saveLocalTodos(todo);
+    // Resetting input
+    e.target.reset();
 });
